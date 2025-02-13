976509e94d91c4029d1eeb51df5d6e8f0c5ff439
Ayala <aldo.ayala@accenture.com>
2025-02-13 11:36:38 -0600

    added cors policies for testing api

--- app/main.py
+++ app/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from app.routers import resumes
 from .locales.localization import Localization, tr
 from .config import client
+from starlette.middleware.cors import CORSMiddleware
 
 Localization.set_language('es')
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
 
 app.include_router(resumes.router, prefix="/api")
 
