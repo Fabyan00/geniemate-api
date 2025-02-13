94b38132acb1c6c770c2c93555e0eaeaac47db91
Ayala <aldo.ayala@accenture.com>
2025-02-13 11:52:17 -0600

    blank spaces deleted

--- app/main.py
+++ app/main.py
@@ -16,10 +16,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(resumes.router, prefix="/api")
